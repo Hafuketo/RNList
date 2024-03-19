365d3289a7a433fb23f41c5c1bfd38f894ef4b34
rick sandstorm <haafket@hotmail.com>
2024-03-19 15:19:14 +0100

    init

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import pokemonList from './data.json';
 
 export default function App() {
   return (
